7ee78af4254b93fefb9062e5b050f04a6d90fb8f
ehisgodswill <ehisgodswill@gmail.com>
2022-10-30 00:30:06 +0100

    added social

--- src/components/Linksection.js
+++ src/components/Linksection.js
@@ -1,12 +1,20 @@
+import slack from './assets/slack.png';
+import github from './assets/github.png'
 function LinkSection() {
     return (
-        <section className='linksection'>
-            <a href='https://training.zuri.team/' id='btn__zuri'></a>
-            <a href='https://books.zuri.team/' id='books'></a>
-            <a href='https://books.zuri.team/python-for-beginners?ref_id=godswillehis' id='book__python'></a>
-            <a href='https://background.zuri.team/' id='pitch'></a>
-            <a href='https://books.zuri.team/design-rules' id='book__design'></a>
-        </section>
+        <>
+            <section className='linksection'>
+                <a href='https://training.zuri.team/' id='twitter_link'>Twitter Link</a>
+                <a href='https://training.zuri.team/' id='btn__zuri'>Zuri Team</a>
+                <a href='https://books.zuri.team/' id='books'>Zuri Books</a>
+                <a href='https://books.zuri.team/python-for-beginners?ref_id=godswillehis' id='book__python'>Python Books</a>
+                <a href='https://background.zuri.team/' id='pitch'>Background Check For Coders</a>
+                <a href='https://books.zuri.team/design-rules' id='book__design'>Design Books</a>
+            </section>
+            <section className='social'>
+
+            </section>
+        </>
     )
 }
 
