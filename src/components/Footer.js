c9f63cc490b5142d76ed373492bf1e6bd902c2ba
ehisgodswill <ehisgodswill@gmail.com>
2022-10-30 00:20:47 +0100

    footer images

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
-import zuri from ''
+import zuri from './assets/Zuri.Internship_Logo.png';
+import i4g from './assets/I4G.png'
 function Footer(){
     return(
         <footer className="footer">
-            <div><img src="" alt="Zuri Training"/></div>
+            <div><img src={zuri} alt="Zuri Training"/></div>
             <div>HNG Internship 9 Frontend Task</div>
-            <div><img src="" alt="Ingresive For Good"/></div>
+            <div><img src={i4g} alt="Ingresive For Good"/></div>
         </footer>
     )
 }
