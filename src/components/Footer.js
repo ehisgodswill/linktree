c57cf2eab11894a9bab2cb97c4b684e37f55df81
ehisgodswill <ehisgodswill@gmail.com>
2022-10-29 20:17:54 +0100

    added asset folder

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+function Footer(){
+    return(
+        <footer className="footer">
+            <div></div>
+            <div>HNG Internship 9 Frontend Task</div>
+            <div></div>
+        </footer>
+    )
+}
+
+export default Footer();
