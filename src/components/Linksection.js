25abd6599f86f62510a8a5ea38d2f41cbc43c8e8
ehisgodswill <ehisgodswill@gmail.com>
2022-10-30 00:11:05 +0100

    linksection

--- src/components/Linksection.js
+++ src/components/Linksection.js
@@ -1,12 +1,11 @@
 function LinkSection() {
     return (
-        <section>
+        <section className='linksection'>
             <a href='https://training.zuri.team/' id='btn__zuri'></a>
             <a href='https://books.zuri.team/' id='books'></a>
-            <a href='https://books.zuri.team/python-for-beginners?ref_id=<yourslackname>' id='book__python'></a>
+            <a href='https://books.zuri.team/python-for-beginners?ref_id=godswillehis' id='book__python'></a>
             <a href='https://background.zuri.team/' id='pitch'></a>
             <a href='https://books.zuri.team/design-rules' id='book__design'></a>
-            <a href='' id=''></a>
         </section>
     )
 }
